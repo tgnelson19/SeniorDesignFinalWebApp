7c7615d2c065b6782aec95c9a7d666390f5230c2
tgnelson19 <nelson.tyler.g@gmail.com>
2023-11-25 15:30:56 -0600

    Continuing to look into potential fixes for current issue

--- sen-des-web-app/src/App.js
+++ sen-des-web-app/src/App.js
@@ -2,30 +2,6 @@ import * as React from 'react';
 import DataTable from './Table';
 import Paper from '@mui/material/Paper';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, onValue } from "firebase/database";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyATXDCNWRDPLG6XBqDW3Q1Snyv7TlYdDh4",
-  authDomain: "seniordesign-9342c.firebaseapp.com",
-  databaseURL: "https://seniordesign-9342c-default-rtdb.firebaseio.com",
-  projectId: "seniordesign-9342c",
-  storageBucket: "seniordesign-9342c.appspot.com",
-  messagingSenderId: "391432450434",
-  appId: "1:391432450434:web:28fa71d79f09e6abd75f9b",
-  measurementId: "G-MMWKSHCQ95"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const db = getDatabase();
-
 const rowsExample = [
   { id: 1, foodName: 'Apple', entryDate: "11/21/2023", expirationDate: "11/30/2023", cost: 0.50},
   { id: 2, foodName: 'Apple', entryDate: "11/21/2023", expirationDate: "11/30/2023", cost: 0.50},
@@ -42,7 +18,6 @@ class App extends React.Component {
       rows: rowsExample
     };
   }
-
 
   /* This is the start of the render routine, or what is presented when the app is rendered */
   render() {
@@ -61,20 +36,14 @@ class App extends React.Component {
             }}
           >
 
-        <DataTable
-          rows={this.state.rows}
-        >
-            
-
-        </DataTable>
+          <DataTable rows={this.state.rows}>
+          </DataTable>
 
 
         </Paper>
       </div>
     )
   }
-
-
 
 }
 
--- sen-des-web-app/src/firebase.js
+++ sen-des-web-app/src/firebase.js
@@ -1,15 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
+require('dotenv').config()
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  
+  apiKey: REACT_APP_APIKEY,
+  authDomain: REACT_APP_AUTHDOMAIN,
+  databaseURL: REACT_APP_DATABASEURL,
+  projectId: REACT_APP_PROJECTID,
+  storageBucket: REACT_APP_STORAGEBUCKET,
+  messagingSenderId: REACT_APP_MESSAGINGSENDERID,
+  appId: REACT_APP_APPID,
+  measurementId: REACT_APP_MEASUREMENTID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getDatabase(app);
